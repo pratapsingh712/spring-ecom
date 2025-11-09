8962c399ff04539ec53cec6b7ec076f53dec2229
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-11-09 20:20:19 +0530

    added controller post method for product details including images

--- src/main/java/com/raghav/SpringEcom/service/ProductService.java
+++ src/main/java/com/raghav/SpringEcom/service/ProductService.java
@@ -4,7 +4,9 @@ import com.raghav.SpringEcom.model.Product;
 import com.raghav.SpringEcom.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -20,5 +22,13 @@ public class ProductService {
     public Product getProductByID(int id) {
         //return productRepo.findById(id).get(); // it's not recommended to use .get()
         return productRepo.findById(id).orElse(new Product(-1));
+    }
+
+    public Product addProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+        return productRepo.save(product);
+
     }
 }
