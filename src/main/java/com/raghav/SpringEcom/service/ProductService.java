4e956813f60f6094c965a90f0abe307acff14743
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-11-09 18:15:43 +0530

    added model, repo, service and controller to handle the /api/products

---
+++ src/main/java/com/raghav/SpringEcom/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.raghav.SpringEcom.service;
+
+import com.raghav.SpringEcom.model.Product;
+import com.raghav.SpringEcom.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+}
